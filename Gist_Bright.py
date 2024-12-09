3ba76a99a86093f022241bb6f1f486f38a09f614
onedering <kloneryga@gmail.com>
2024-12-09 11:27:51 +0300

    update

--- Gist_Bright.py
+++ Gist_Bright.py
@@ -1,43 +1,56 @@
-# установить библиотеки
+# Установка необходимых библиотек
 # pip install opencv-python
 # pip install matplotlib
-# изображения должны находиться в одной папке с программой
 
 import cv2
 from matplotlib import pyplot as plt
 
-a = ["43.bmp", "43msg100%-LSB.bmp", "43msg50%-LSB.bmp", "43msg10%-LSB.bmp",
-     "43msg1%-LSB.bmp"]  # Ввести номера изображений в нужном порядке
+# Список изображений
+image_files = ["43.bmp", "43msg100%-LSB.bmp", "43msg50%-LSB.bmp",
+               "43msg10%-LSB.bmp", "43msg1%-LSB.bmp"]
 
+def gist(image_path, output_folder="output"):
+    """Строит гистограмму для изображения."""
+    try:
+        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        if img is None:
+            raise FileNotFoundError(f"Изображение {image_path} не найдено.")
+        
+        plt.figure()
+        plt.title(f'Гистограмма {image_path}')
+        plt.hist(img.ravel(), bins=200, range=[0, 256])
+        plt.xlabel("Яркость")
+        plt.ylabel("Количество пикселей")
 
-def gist(imageN):
-    # img = cv2.imread(imageN, 0)
-    # histr = cv2.calcHist([img], [0], None, [256], [0, 256])
-    # plt.title('Гистограмма ' + a[i])
-    # plt.hist(histr, bins=200)
-    # # plt.plot(histr, kind='hist')
-    # # Автосейв без показа гистограммы
-    # # plt.savefig('Гистограмма ' + a[i] + ".jpg")
-    # plt.show()
-    img = cv2.imread(imageN, cv2.IMREAD_GRAYSCALE)
-    plt.title('Гистограмма ' + a[i])
-    plt.hist(img.ravel(), 200, [0, 256])
-    # plt.savefig('Гистограмма ' + a[i] + ".jpg", edgecolor='b', dpi=300)
-    img = 0
-    plt.show()
+        # Сохранение гистограммы
+        output_path = f"{output_folder}/Гистограмма_{image_path.split('.')[0]}.png"
+        plt.savefig(output_path, dpi=300)
+        plt.close()  # Закрытие графика для экономии памяти
 
+    except Exception as e:
+        print(f"Ошибка при создании гистограммы для {image_path}: {e}")
 
+def matrix(image_path):
+    """Печатает матрицу яркостей для изображения."""
+    try:
+        img = cv2.imread(image_path)
+        if img is None:
+            raise FileNotFoundError(f"Изображение {image_path} не найдено.")
+        
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        value = img_hsv[:, :, 2]
+        print(f"Матрица яркости для {image_path}:")
+        print(value)
+    except Exception as e:
+        print(f"Ошибка при обработке матрицы {image_path}: {e}")
 
-def matrix(imageN):
-    img = cv2.imread(imageN)
-    # cv2.imshow('image', img)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    value = img_hsv[:, :, 2]
-    print("matrix ", a[i])
-    print(value)
+def process_images(image_list):
+    """Обрабатывает список изображений."""
+    for image_path in image_list:
+        print(f"Обработка {image_path}...")
+        matrix(image_path)
+        gist(image_path)
 
-
-for i in range(5):
-    matrix(a[i])
-    gist(a[i])
-    i += 1
+# Запуск обработки
+if __name__ == "__main__":
+    process_images(image_files)
--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# Gist_bright
-#Программа для вывода гистограммы и матрицы яркости
-# установить библиотеки
-# pip install opencv-python
-# pip install matplotlib
-# изображения должны находиться в одной папке с программой
+   # Gist_Bright
+Gist_Bright: Программа для построения гистограмм и анализа матрицы яркости изображений.
+
+## Установка
+1. Клонируйте репозиторий:
+   git clone https://github.com/onedering/Gist_Bright
+   cd Gist_Bright
+
+2. Установите зависимости:
+  pip install opencv-python matplotlib
+  pip install opencv-python
+
+## Использование
+1. Поместите изображения в ту же папку, где находится скрипт.
+2. Запустите программу:
+   python gist_bright.py
+
+3. Результаты:
+    Гистограммы сохраняются в папке `output`.
+    Матрицы яркости выводятся в консоль.
